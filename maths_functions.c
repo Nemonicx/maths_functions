b70061ae8dbbe3d9bfa7b47254230771edd14948
Naim Maoun <P15209971@GWHXX651WIN019W.lec-admin.dmu.ac.uk>
2017-01-31 15:13:52 +0000

    The program can now divide aswell

--- maths_functions.c
+++ maths_functions.c
@@ -4,8 +4,20 @@ int main()
 {
 	int x;
 	int y;
+	printf("Additions\n");
 	scanf("%i %i", &x,&y);
 	printf( "%i + %i = %i \n", x, y, x+y);
 	//scanf("%i %i", &x,&y);
+	printf("Substraction\n");
+        scanf("%i %i", &x,&y);
+        printf( "%i - %i = %i \n", x, y, x-y);
+	printf("Multiplication\n");
+        scanf("%i %i", &x,&y);
+        printf( "%i * %i = %i \n", x, y, x*y);
+	printf("Division\n");
+        scanf("%i %i", &x,&y);
+        printf( "%i / %i = %i \n", x, y, x/y);
+
 	return 0;
+
 }
